fe50e232c79a20c5e25fff145226fc1ea5cea954
OpiskelijaMiika <miika.kangas@seamk.fi>
2023-01-26 18:59:18 +0200

    Muutettu main sijainti

--- sensors_main.py
+++ sensors_main.py
@@ -2,20 +2,6 @@ import sys
 #Ensimmäinen muutos.
 # This is the main finction for controlling the flow of the
 # implementation.
-def main():
-    limits = parse_limits()
-    sensor_data = []
-#Toinen
-    if len(limits) > 0 and check_limits(limits):
-        sensor_data = read_sensors()
-        # This is a mockup code that prints the sensor readings
-        # to console. To be replaced with actual implementation
-        # (whatever that might be according to the low level design,
-        # for example).
-        for row in sensor_data:
-            print (row)
-    else:
-        print("Error: Incorrect command line arguments.")
 
 # This is the parse_limits function for getting the temperature
 # limits from the command line parameters. Returns an array
