1544137dc496f10152b343dd17d59f15118a38c8
Sneeqi <santeri.kaaranto@gmail.com>
2023-01-26 15:53:03 +0200

    Siirretty main funktio modulin loppuun

--- sensors_main.py
+++ sensors_main.py
@@ -1,21 +1,5 @@
 import sys
 # ensimmäinen muutos
-# This is the main function for controlling the flow of the
-# implementation.
-def main():
-    limits = parse_limits()
-    sensor_data = []
-
-    if len(limits) > 0 and check_limits(limits):
-        sensor_data = read_sensors()
-        # This is a mockup code that prints the sensor readings
-        # to console. To be replaced with actual implementation
-        # (whatever that might be according to the low level design,
-        # for example).
-        for row in sensor_data:
-            print (row)
-    else:
-        print("Error: Incorrect command line arguments.")
 
 # This is the parse_limits function for getting the temperature
 # limits from the command line parameters. Returns an array
@@ -55,6 +39,23 @@ def read_sensors():
 
 # Other parts of the implementation such as printing the information
 # for the operator are also missing and to be implemented.
+
+# This is the main function for controlling the flow of the
+# implementation.
+def main():
+    limits = parse_limits()
+    sensor_data = []
+
+    if len(limits) > 0 and check_limits(limits):
+        sensor_data = read_sensors()
+        # This is a mockup code that prints the sensor readings
+        # to console. To be replaced with actual implementation
+        # (whatever that might be according to the low level design,
+        # for example).
+        for row in sensor_data:
+            print (row)
+    else:
+        print("Error: Incorrect command line arguments.")
 
 if __name__ == "__main__":
     main()
