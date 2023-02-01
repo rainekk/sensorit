f04df55c4420cce7c1d8e5109984c73b269e22f7
janneSeamkStudent <Janne.Yrttimaa@seamk.fi>
2023-02-01 12:46:18 +0200

    lisatty 'print'

--- sensors_main.py
+++ sensors_main.py
@@ -16,6 +16,7 @@ def main():
             print (row)
             print('test3')
             print("test5")
+            print("testi6")
     else:
         print("Error: Incorrect command line arguments.")
 
